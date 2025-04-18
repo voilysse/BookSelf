3f916a768f61e64e7bdc8dbb02d9fa7a3b2d6427
Yana <60840308+yanakm@users.noreply.github.com>
2025-04-18 16:09:55 +0300

    login/register fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import Home from './components/Home';
 import Login from './components/Login.jsx';
 import Register from './components/Register';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 function App() {
   const [isRegisterVisible, setIsRegisterVisible] = useState(false);
+  const [isLoginVisible, setIsLoginVisible] = useState(false);
   return (
     <BrowserRouter>
       <Routes>
-         <Route path="/" element ={<Home isRegisterVisible={isRegisterVisible} setIsRegisterVisible={setIsRegisterVisible}/>} />
+         <Route path="/" element ={<Home isLoginVisible={isLoginVisible} setIsLoginVisible={setIsLoginVisible}
+         isRegisterVisible={isRegisterVisible} setIsRegisterVisible={setIsRegisterVisible}/>} />
          <Route path="/login" element={<Login/>}/>
          <Route path="/register" element={<Register/>}/>
         </Routes>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,37 @@
-
-function Login() {
-
+function Login({ setIsLoginVisible }) {
+  return (
+    <>
+      <div className="Register">
+        <div className="Container">
+          <div className="CloseButton">
+            <button onClick={() => setIsLoginVisible(false)}>
+              <i className="fa-solid fa-xmark"></i>
+            </button>
+          </div>
+          <img className="Logo" src="/images/logo.png" alt="Bookself Logo" />
+          <div className="Title">
+            <h1 className="Typewriter Twelve">Welcome back</h1>
+            <div className="ParagraphContainer">
+              <p className="SmallGrey" style={{ marginRight: "20px" }}>
+                Continue exploring the vast world of literature
+              </p>
+            </div>
+          </div>
+          <div className="Form">
+            <h2>Email</h2>
+            <input className="Input" type="email"></input>
+            <h2>Password</h2>
+            <input className="Input" type="password"></input>
+          </div>
+          <button className="OrangeButton">Log in</button>
+          <div className="textSeparator">
+            <hr className="separator" />
+          </div>
+          <p className="SmallGrey">Forgot your password?</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
