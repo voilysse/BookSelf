e88d2e4925c1377ae109e509bf91c3d594585a4c
Yana <60840308+yanakm@users.noreply.github.com>
2025-07-29 15:42:20 +0300

    Board updates

--- client/src/components/users/UserShelves.jsx
+++ client/src/components/users/UserShelves.jsx
@@ -2,24 +2,101 @@ import { useParams } from "react-router-dom";
 import { FollowButton, BlockButton } from "../Button/Button";
 import BookshelvesTab from "./BookshelvesPage/BookshelvesTab";
 import { Link } from "react-router-dom";
-import { FaRegBookmark, FaSyncAlt, FaRegCheckCircle } from "react-icons/fa";
+import {
+  FaRegBookmark,
+  FaSyncAlt,
+  FaRegCheckCircle,
+  FaTrashAlt,
+} from "react-icons/fa";
 import BookCardLarge from "../books/BookCardLarge";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useGetUserShelvesByNameQuery } from "../../features/shelfApi";
 import {
   useGetBookQuery,
   useGetBookReviewsQuery,
+  useGetUserReviewsQuery,
 } from "../../features/bookApi.js";
 import StarRating from "../books/StarRating";
-
+import { createPortal } from "react-dom";
 import { PrimaryButton } from "../Button/Button";
-import { FaList } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaPen, FaList, FaTrash } from "react-icons/fa";
 import { FiHeart } from "react-icons/fi";
-
 import { MdGridView } from "react-icons/md";
+import { motion, AnimatePresence } from "framer-motion";
+import ReviewModal from "../books/ReviewModal";
 const SidebarNavigation = () => {
   return <div className="w-60 h-full fixed l-0 my-2 shadow-md"></div>;
+};
+
+const DeleteModal = ({ setAllowed, book, shelf, isOpen, onClose }) => {
+  const modalRef = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        onClose();
+      }
+    };
+    const handleEscape = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("keydown", handleEscape);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, [isOpen, onClose]);
+  let shelfName = "";
+  switch (shelf) {
+    case "wtr":
+      shelfName = "Want to read";
+      break;
+    case "reading":
+      shelfName = "Reading";
+      break;
+    case "read":
+      shelfName = "Read";
+      break;
+  }
+  return createPortal(
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+      <div
+        ref={modalRef}
+        className="bg-white rounded-2xl shadow-2xl p-6 w-1/2 max-w-xl transition-all duration-200 ease-in-out flex justify-center flex-col items-center"
+      >
+        <FaTrashAlt className="text-xl m-3 text-rat_dark" />
+        <h2 className="text-rat_base text-center">
+          Do you want to remove <br />
+          <span className="font-semibold text-rat_dark">{book.title}</span> from
+          your <span className="font-semibold text-rat_dark">{shelfName} </span>
+          bookshelf?
+        </h2>
+        <div className="flex gap-4 mt-3 ">
+          <button
+            onClick={() => {
+              onClose();
+              setAllowed(true);
+            }}
+            className="flex bg-white border border-solid border-rat_base p-1 w-24 rounded-md hover:bg-rat_base hover:opacity-75 hover:text-white text-rat_dark items-center justify-center"
+          >
+            Remove
+          </button>
+          <button
+            onClick={() => onClose()}
+            className="flex bg-rat_base opacity-75 p-1 w-24 rounded-md hover:opacity-100 text-white justify-center"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
 };
 
 const Activity = () => {
@@ -29,16 +106,50 @@ const Activity = () => {
     useGetUserShelvesByNameQuery("Read");
   const { data: wantToRead, isLoading: wantToReadIsLoading } =
     useGetUserShelvesByNameQuery("Want to Read");
-  const { data: favourites, isLoading: favouritesIsLoading } =
-    useGetUserShelvesByNameQuery("Favourites");
-  if (
-    favouritesIsLoading ||
-    readingIsLoading ||
-    readIsLoading ||
-    wantToReadIsLoading
-  )
-    return <p>Loading forums...</p>;
+  const [readingList, setReadingList] = useState([]);
+  const [readList, setReadList] = useState([]);
+  const [wtrList, setWtrList] = useState([]);
+  useEffect(() => {
+    if (reading) setReadingList(reading.shelf.books || []);
+  }, [reading]);
 
+  useEffect(() => {
+    if (read) setReadList(read.shelf.books || []);
+  }, [read]);
+
+  useEffect(() => {
+    if (wantToRead) setWtrList(wantToRead.shelf.books || []);
+  }, [wantToRead]);
+  if (readingIsLoading || readIsLoading || wantToReadIsLoading)
+    return <p>Loading forums...</p>;
+  {
+    /*HANDLERS*/
+  }
+  const handleReading = (id) => {
+    const newBook = wtrList.filter((book) => book._id === id);
+    setWtrList((prevList) => prevList.filter((book) => book._id !== id));
+    setReadingList((prevList) => [newBook[0], ...prevList]);
+  };
+  const handleRead = (id, from) => {
+    const list = from === "wtr" ? wtrList : readingList;
+    const newBook = list.filter((book) => book._id === id);
+    if (from === "wtr")
+      setWtrList((prevList) => prevList.filter((book) => book._id !== id));
+    if (from === "reading")
+      setReadingList((prevList) => prevList.filter((book) => book._id !== id));
+    setReadList((prevList) => [newBook[0], ...prevList]);
+  };
+  const handleDelete = (id, from) => {
+    if (from === "wtr") {
+      setWtrList((prevList) => prevList.filter((book) => book._id !== id));
+    }
+    if (from === "reading") {
+      setReadingList((prevList) => prevList.filter((book) => book._id !== id));
+    }
+    if (from === "read") {
+      setReadList((prevList) => prevList.filter((book) => book._id !== id));
+    }
+  };
   return (
     <div className=" w-full mx-auto mt-2 flex justify-center items-center">
       <div className="flex flex-col w-full h-screen">
@@ -51,11 +162,29 @@ const Activity = () => {
               <FaRegBookmark className="m-1" />
               Want to read
             </div>
-            <div className="gap-3 w-full flex flex-col items-center">
-              {" "}
-              {favourites.shelf.books.map((b) => (
-                <BookCardShelfWantToRead book={b} />
-              ))}
+            <div className=" transition-all duration-300 gap-3 w-full flex flex-col items-center">
+              <AnimatePresence>
+                {wtrList.map((b) => (
+                  <motion.div
+                    key={b._id}
+                    layout
+                    initial={{ opacity: 0, y: 0, x: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 20 }}
+                    transition={{ type: "spring", stiffness: 300, damping: 20 }}
+                    className="w-full"
+                  >
+                    {" "}
+                    <BookCardBoard
+                      book={b}
+                      shelf={"wtr"}
+                      handleDelete={handleDelete}
+                      handleRead={handleRead}
+                      handleReading={handleReading}
+                    />
+                  </motion.div>
+                ))}
+              </AnimatePresence>
             </div>
           </div>
           {/*Reading*/}
@@ -64,12 +193,59 @@ const Activity = () => {
               <FaSyncAlt className="m-1" />
               Reading
             </div>
+            <div className=" transition-all duration-300 gap-3 w-full flex flex-col items-center">
+              <AnimatePresence>
+                {readingList.map((b) => (
+                  <motion.div
+                    key={b._id}
+                    layout
+                    initial={{ opacity: 0, y: 0, x: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 20 }}
+                    transition={{ type: "spring", stiffness: 300, damping: 20 }}
+                    className="w-full"
+                  >
+                    <BookCardBoard
+                      book={b}
+                      shelf={"reading"}
+                      handleDelete={handleDelete}
+                      handleRead={handleRead}
+                      handleReading={handleReading}
+                    />
+                  </motion.div>
+                ))}
+              </AnimatePresence>
+            </div>
           </div>
           {/*Read*/}
           <div className="hidden  flex-col items-center lg:flex rounded-xl max-h-[550px] lg:w-1/3 bg-rat_lightest">
             <div className="m-2 text-md text-rat_dark border border-solid border-rat_dark w-40 h-8 flex justify-center items-center rounded-[20px]">
               <FaRegCheckCircle className="m-1" />
               Read
+            </div>
+            <div className=" transition-all duration-300 gap-3 w-full flex flex-col items-center">
+              <AnimatePresence>
+                {readList.map((b) => (
+                  <motion.div
+                    key={b._id}
+                    layout
+                    initial={{ opacity: 0, y: 0, x: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 20 }}
+                    transition={{ type: "spring", stiffness: 300, damping: 20 }}
+                    className="w-full"
+                  >
+                    {" "}
+                    <BookCardBoard
+                      book={b}
+                      shelf={"read"}
+                      handleDelete={handleDelete}
+                      handleRead={handleRead}
+                      handleReading={handleReading}
+                    />
+                  </motion.div>
+                ))}
+              </AnimatePresence>
             </div>
           </div>
         </div>
@@ -327,25 +503,55 @@ const BookCardList = ({ book }) => {
   );
 };
 
-const BookCardShelfWantToRead = ({ book }) => {
+const BookCardBoard = ({
+  book,
+  shelf,
+  handleDelete,
+  handleRead,
+  handleReading,
+}) => {
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [deleteAllowed, setDeleteAllowed] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [review, setReview] = useState(false);
+  const { user } = useSelector((state) => state.auth);
+  const userId = user?._id;
+  const { data: reviews, isLoading: reviewsLoading } =
+    useGetUserReviewsQuery(userId);
   const { data, isLoading } = useGetBookQuery(book._id);
   const { data: reviewData } = useGetBookReviewsQuery(book._id);
-
   const avgRating =
     reviewData?.reviews?.length > 0
       ? reviewData.reviews.reduce((sum, r) => sum + r.rating, 0) /
         reviewData.reviews.length
       : 0;
-  if (isLoading) {
+  useEffect(() => {
+    if (reviews && book?._id) {
+      const reviewsOnBook = reviews.reviews.filter(
+        (r) => r.book._id === book._id
+      );
+      setReview(() => reviewsOnBook[0]);
+    }
+  }, [reviews, book]);
+  useEffect(() => {
+    setTimeout(() => {
+      if (deleteAllowed) {
+        handleDelete(book._id, shelf);
+      }
+    }, 400);
+  }, [deleteAllowed]);
+  if (isLoading || reviewsLoading) {
     return <div>Loading...</div>;
   }
   return (
     <div
-      className="group flex-col w-11/12 h-28 hover:h-40
+      className={`group flex-col w-11/12 h-28 ${
+        review && shelf === "read" ? "hover:h-48" : "hover:h-40"
+      }
     rounded-lg border border-solid
      border-rat_lightest 
      drop-shadow-sm shadow-rat_dark
-      bg-white transition-all duration-400 ease-in-out flex"
+      bg-white transition-all duration-400 ease-in-out flex`}
     >
       <div className="flex">
         {/*LEFT: COVER*/}
@@ -360,7 +566,7 @@ const BookCardShelfWantToRead = ({ book }) => {
         </div>
 
         {/*RIGHT: TITLE*/}
-        <div className="flex flex-col mt-3">
+        <div className="flex flex-col mt-3 w-1/2">
           <Link
             key={book._id}
             to={`/books/${book._id}`}
@@ -388,15 +594,66 @@ const BookCardShelfWantToRead = ({ book }) => {
       </div>
       {/*Buttons*/}
       <div className="px-2 w-full h-10 self-center flex justify-end  gap-2 items-center  opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-        <button className="flex bg-white border border-solid border-rat_base p-1 w-24 rounded-md hover:bg-rat_base hover:opacity-75 hover:text-white text-rat_dark">
-          <FaSyncAlt className="text-sm m-1" />
-          Reading
-        </button>
-        <button className="flex bg-rat_base opacity-75 p-1 w-24 rounded-md hover:opacity-100 text-white">
-          <FaRegCheckCircle className="m-1" />
-          Read it
-        </button>
+        {shelf === "wtr" && (
+          <button
+            onClick={() => handleReading(book._id)}
+            className="flex bg-white border border-solid border-rat_base p-1 w-24 rounded-md hover:bg-rat_base hover:opacity-75 hover:text-white text-rat_dark"
+          >
+            <FaSyncAlt className="text-sm m-1" />
+            Reading
+          </button>
+        )}
+        {(shelf === "wtr" || shelf === "reading") && (
+          <button
+            onClick={() => handleRead(book._id, shelf)}
+            className="flex bg-rat_base opacity-75 p-1 w-24 rounded-md hover:opacity-100 text-white"
+          >
+            <FaRegCheckCircle className="m-1" />
+            Read it
+          </button>
+        )}
+        {shelf === "read" && !review && (
+          <button
+            onClick={() => setModalOpen(true)}
+            className="flex bg-rat_base opacity-75 p-1 w-36 rounded-md hover:opacity-100 text-white"
+          >
+            <FaPen className="text-sm m-1" />
+            Leave a review
+          </button>
+        )}
+        {shelf === "read" && review && (
+          <div className="overflow-hidden w-full h-16 absolute bottom-0 m-2  flex flex-col items-end">
+            <h3 className="text-xs text-rat_base">Your review on this book</h3>
+            <StarRating rating={review.rating} size={55} />
+            <span className="block text-right w-10/12 text-xs text-rat_dark mt-1 ">
+              "{review.text}"
+            </span>
+          </div>
+        )}
       </div>
+      {/*Deleting*/}
+      <button
+        onClick={() => {
+          setDeleteOpen(true);
+        }}
+        className="absolute right-0 top-0 m-2 opacity-0 group-hover:opacity-100"
+      >
+        <FaTrash className="text-rat_light hover:text-rat_base" />
+      </button>
+      <ReviewModal
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        bookId={book}
+      />
+      {deleteOpen && (
+        <DeleteModal
+          setAllowed={setDeleteAllowed}
+          book={book}
+          shelf={shelf}
+          isOpen={deleteOpen}
+          onClose={() => setDeleteOpen(false)}
+        />
+      )}
     </div>
   );
 };
